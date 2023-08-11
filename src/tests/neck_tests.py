0721b61fed88e54fc0ad1bf22b4d05ca4c35c3d7
klemen1999 <klemenskrlj8@gmail.com>
2023-08-11 13:30:21 +0200

    added tests for neck inference

---
+++ src/tests/neck_tests.py
@@ -0,0 +1,53 @@
+import unittest
+import torch
+from luxonis_train.models.necks import *
+from luxonis_train.utils.general import dummy_input_run
+
+
+class NeckTestCases(unittest.TestCase):
+    def test_reppan_inference(self):
+        """Tests inference on RepPAN neck with EfficienRep backbone. Output of forward should be List[torch.Tensor]"""
+        from luxonis_train.models.backbones import EfficientRep
+
+        input_shapes = [[1, 3, 256, 256], [1, 3, 512, 256]]
+
+        backbone = EfficientRep(
+            channels_list=[64, 128, 256, 512, 1024],
+            num_repeats=[1, 6, 12, 18, 6],
+            is_4head=True,
+        )
+
+        for input_shape in input_shapes:
+            for num_heads in [2, 3, 4]:
+                with self.subTest(input_shape=input_shape, num_heads=num_heads):
+                    input = torch.zeros(input_shape)
+                    prev_out_shapes = dummy_input_run(backbone, input_shape)
+                    neck = RepPANNeck(
+                        prev_out_shapes=prev_out_shapes, num_heads=num_heads
+                    )
+
+                    outs = backbone(input)
+                    outs = neck(outs)
+
+                    self.assertIsInstance(outs, list)
+                    for out in outs:
+                        self.assertIsInstance(out, torch.Tensor)
+
+    def test_reppan_incorrect_num_heas(self):
+        """Tests num_heads parameter of RepPAN neck"""
+        prev_out_shapes = [
+            [1, 32, 56, 56],
+            [1, 64, 28, 28],
+            [1, 128, 14, 14],
+            [1, 256, 7, 7],
+        ]
+        for num_heads in [1, 5, -1]:
+            with self.subTest(num_heads=num_heads):
+                with self.assertRaises(ValueError):
+                    neck = RepPANNeck(
+                        prev_out_shapes=prev_out_shapes, num_heads=num_heads
+                    )
+
+
+if __name__ == "__main__":
+    unittest.main()

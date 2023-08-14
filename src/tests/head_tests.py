c6d90e85e43d0c5499aaf1e4fb322d02ef6f7d75
klemen1999 <klemenskrlj8@gmail.com>
2023-08-14 21:08:41 +0200

    added inference test for yolov6 head

--- src/tests/head_tests.py
+++ src/tests/head_tests.py
@@ -47,6 +47,51 @@ class HeadTestCases(unittest.TestCase):
 
                     self.assertIsInstance(outs, torch.Tensor)
 
+    def test_yolov6_head(self):
+        """Tests YoloV6 head together with EfficienRep backbone and RepPANNeck"""
+        from luxonis_train.models.backbones import EfficientRep
+        from luxonis_train.models.necks import RepPANNeck
+
+        input_shapes = [[1, 3, 256, 256], [1, 3, 512, 256]]
+        backbone = EfficientRep()
+
+        num_heads_offset_pairs = [(2, [0, 1, 2]), (3, [0, 1]), (4, [0])]
+
+        for input_shape in input_shapes:
+            for num_heads, offsets in num_heads_offset_pairs:
+                for offset in offsets:
+                    input = torch.zeros(input_shape)
+                    input_channels_shapes = dummy_input_run(backbone, input_shape)
+                    backbone.eval()
+                    neck = RepPANNeck(
+                        input_channels_shapes=input_channels_shapes, num_heads=num_heads
+                    )
+                    input_channels_shapes = dummy_input_run(
+                        neck, input_channels_shapes, multi_input=True
+                    )
+                    neck.eval()
+
+                    head = YoloV6Head(
+                        n_classes=10,
+                        input_channels_shapes=input_channels_shapes,
+                        original_in_shape=input_shape,
+                        num_heads=num_heads,
+                        offset=offset,
+                    )
+
+                    outs = backbone(input)
+                    outs = neck(outs)
+                    outs = head(outs)
+
+                    self.assertIsInstance(outs, list)
+                    self.assertEqual(len(outs), 3)
+
+                    self.assertIsInstance(outs[0], list)
+                    for out in outs[0]:
+                        self.assertIsInstance(out, torch.Tensor)
+                    self.assertIsInstance(outs[1], torch.Tensor)
+                    self.assertIsInstance(outs[2], torch.Tensor)
+
 
 if __name__ == "__main__":
     unittest.main()

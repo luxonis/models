bcfd722a415a936d4512eb2bf1df374a02344ab5
peskaf <hsfp52@gmail.com>
2023-09-14 18:05:21 +0200

    Renamed inputs.

--- tests/unittests/test_utils/test_losses/test_focal_loss.py
+++ tests/unittests/test_utils/test_losses/test_focal_loss.py
@@ -8,19 +8,19 @@ torch.manual_seed(42)
 
 def test_focal_loss_min():
     loss_fn = FocalLoss(alpha=1.0, gamma=1, use_sigmoid=False)
-    inputs = torch.tensor([0.0, 0.0, 1.0, 1.0, 0.0], requires_grad=True)
+    predictions = torch.tensor([0.0, 0.0, 1.0, 1.0, 0.0], requires_grad=True)
     targets = torch.tensor([0, 0, 1, 1, 0], dtype=torch.float32)
 
-    loss = loss_fn(inputs, targets)
+    loss = loss_fn(predictions, targets)
 
     assert torch.allclose(loss, torch.tensor(0.0))
 
 
 def test_focal_loss_max():
     loss_fn = FocalLoss(alpha=1.0, gamma=1, use_sigmoid=False)
-    inputs = torch.tensor([0.0, 0.0, 1.0, 1.0, 0.0], requires_grad=True)
+    predictions = torch.tensor([0.0, 0.0, 1.0, 1.0, 0.0], requires_grad=True)
     targets = torch.tensor([1, 1, 0, 0, 1], dtype=torch.float32)
-    loss = loss_fn(inputs, targets)
+    loss = loss_fn(predictions, targets)
 
     # torch clips negative infinity that log(0.0) would produce to -100; also exp{-100} is almost zero.
     assert torch.allclose(loss, torch.tensor(100.0))
@@ -32,9 +32,9 @@ def test_focal_loss_shapes():
     loss_fn = FocalLoss(use_sigmoid=False)
 
     for shape in shapes:
-        inputs = torch.rand(shape, requires_grad=True)
+        predictions = torch.rand(shape, requires_grad=True)
         targets = torch.randint(0, 2, shape)
-        loss = loss_fn(inputs, targets)
+        loss = loss_fn(predictions, targets)
         assert isinstance(loss, torch.Tensor)
         assert loss.shape == torch.Size([])  # Loss should be a scalar
 
@@ -45,9 +45,9 @@ def test_focal_loss_shapes_with_sigmoid():
     loss_fn = FocalLoss(use_sigmoid=True)
 
     for shape in shapes:
-        inputs = torch.rand(shape, requires_grad=True) * 100 - 50
+        predictions = torch.rand(shape, requires_grad=True) * 100 - 50
         targets = torch.randint(0, 2, shape)
-        loss = loss_fn(inputs, targets)
+        loss = loss_fn(predictions, targets)
         assert isinstance(loss, torch.Tensor)
         assert loss.shape == torch.Size([])  # Loss should be a scalar
 
@@ -56,14 +56,14 @@ def test_focal_loss_sigmoid():
     shape = (8, 64, 64, 1)
 
     # values between -50 and 50
-    inputs = torch.rand(shape, requires_grad=True) * 100 - 50
+    predictions = torch.rand(shape, requires_grad=True) * 100 - 50
     targets = torch.randint(0, 2, shape)
 
     loss_fn_sig = FocalLoss(use_sigmoid=True)
     loss_fn_no_sig = FocalLoss(use_sigmoid=False)
 
-    result_sig = loss_fn_sig(inputs, targets)
-    result_no_sig = loss_fn_no_sig(torch.nn.functional.sigmoid(inputs), targets)
+    result_sig = loss_fn_sig(predictions, targets)
+    result_no_sig = loss_fn_no_sig(torch.nn.functional.sigmoid(predictions), targets)
     assert torch.allclose(result_sig, result_no_sig)
 
 
@@ -73,11 +73,11 @@ def test_focal_loss_forgotten_sigmoid():
     shape = (8, 64, 64, 1)
 
     # values between -50 and 50
-    inputs = torch.rand(shape, requires_grad=True) * 100.0 - 50.0
+    predictions = torch.rand(shape, requires_grad=True) * 100.0 - 50.0
     targets = torch.randint(0, 2, shape)
 
     with pytest.raises(RuntimeError):
-        loss_fn(inputs, targets)
+        loss_fn(predictions, targets)
 
 
 if __name__ == "__main__":

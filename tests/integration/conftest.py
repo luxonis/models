0b9a1551320c93e5fa151ee5949bd632b84d7fea
Martin Kozlovsky <kozlovsky.m7@gmail.com>
2023-12-13 11:33:32 +0100

    logging fails

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -5,9 +5,21 @@ import pytest
 
 @pytest.fixture(scope="session", autouse=True)
 def create_coco_dataset():
-    subprocess.run(["nbscript", "examples/COCO_people_dataset.ipynb"])
+    result = subprocess.run(
+        ["nbscript", "examples/COCO_people_dataset.ipynb"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    print(result.stdout.decode("utf-8"))
+    print(result.stderr.decode("utf-8"))
 
 
 @pytest.fixture(scope="session", autouse=True)
 def create_cifar10_dataset():
-    subprocess.run(["nbscript", "examples/CIFAR10_dataset.ipynb"])
+    result = subprocess.run(
+        ["nbscript", "examples/CIFAR10_dataset.ipynb"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    print(result.stdout.decode("utf-8"))
+    print(result.stderr.decode("utf-8"))

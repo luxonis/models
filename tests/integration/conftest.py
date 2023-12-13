296a9bfd26d61fdeb3c6399648776c24f881ebf1
Martin Kozlovsky <kozlovsky.m7@gmail.com>
2023-12-13 11:25:12 +0100

    reverted

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,23 +1,13 @@
-import os
 import subprocess
-from pathlib import Path
 
 import pytest
 
-TEST_LUXONIS_ML_BASE_PATH = Path("tests/luxonis_ml")
-
-
-@pytest.fixture(scope="session")
-def prepare_environment():
-    TEST_LUXONIS_ML_BASE_PATH.mkdir(parents=True, exist_ok=True)
-    os.environ["LUXONISML_BASE_PATH"] = str(TEST_LUXONIS_ML_BASE_PATH)
-
 
 @pytest.fixture(scope="session", autouse=True)
-def create_coco_dataset(prepare_environment):
+def create_coco_dataset():
     subprocess.run(["nbscript", "examples/COCO_people_dataset.ipynb"])
 
 
 @pytest.fixture(scope="session", autouse=True)
-def create_cifar10_dataset(prepare_environment):
+def create_cifar10_dataset():
     subprocess.run(["nbscript", "examples/CIFAR10_dataset.ipynb"])

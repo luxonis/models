35b3859569a928a4e0b1044f25d4db83ad9f236e
peskaf <filip@peska.me>
2023-12-14 11:05:53 +0100

    Add NMS unit tests.

--- tests/unittests/test_utils/test_boxutils.py
+++ tests/unittests/test_utils/test_boxutils.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 from luxonis_train.utils.boxutils import (
     anchors_for_fpn_features,
@@ -5,6 +6,7 @@ from luxonis_train.utils.boxutils import (
     bbox_iou,
     compute_iou_loss,
     dist2bbox,
+    non_max_suppression,
     process_bbox_predictions,
     process_keypoints_predictions,
 )
@@ -113,3 +115,68 @@ def test_anchors_for_fpn_features():
     assert isinstance(n_anchors_list, list)
     assert isinstance(stride_tensor, torch.Tensor)
     assert len(n_anchors_list) == len(features)
+
+
+def test_non_max_suppression():
+    # Initialize test data
+    n_classes = 5
+    bs = 4
+    preds = torch.rand(bs, 100, 6)  # Simulate predictions with shape [batch_size, N, M]
+    conf_thres = 0.25
+    iou_thres = 0.45
+    max_det = 10
+
+    # Test valid input
+    result = non_max_suppression(
+        preds, n_classes, conf_thres=conf_thres, iou_thres=iou_thres, max_det=max_det
+    )
+    assert isinstance(result, list)
+    assert all(isinstance(tensor, torch.Tensor) for tensor in result)
+    assert all(tensor.shape[1] == preds.shape[-1] for tensor in result)
+    assert all(tensor.size(0) <= max_det for tensor in result)
+
+    # Test invalid confidence threshold
+    with pytest.raises(ValueError):
+        non_max_suppression(preds, n_classes, conf_thres=1.5)
+
+    # Test invalid IoU threshold
+    with pytest.raises(ValueError):
+        non_max_suppression(preds, n_classes, iou_thres=-0.1)
+
+    # Test bbox format conversion
+    for bbox_format in ["xyxy", "xywh", "cxcywh"]:
+        result_format = non_max_suppression(preds, n_classes, bbox_format=bbox_format)  # type: ignore
+        assert isinstance(result_format, list)
+
+    # Test agnostic mode
+    result_agnostic = non_max_suppression(preds, n_classes, agnostic=True)
+    assert isinstance(result_agnostic, list)
+
+    # Test non-agnostic mode
+    result_non_agnostic = non_max_suppression(preds, n_classes, agnostic=False)
+    assert isinstance(result_non_agnostic, list)
+
+    # Test real scenario
+    n_classes = 1
+    conf_thres = 0.25
+    iou_thres = 0.45
+    max_det = 300
+
+    # Define bounding boxes and scores (x1, y1, x2, y2, score, class)
+    # Two boxes overlap, one with higher confidence should be kept
+    box1 = [0, 0, 50, 50, 0.9, 0]  # High confidence box
+    box2 = [10, 10, 60, 60, 0.6, 0]  # Overlapping box with lower confidence
+    box3 = [100, 100, 150, 150, 0.8, 0]  # Non-overlapping box
+
+    # Simulate predictions tensor
+    preds = torch.tensor([[box1, box2, box3]])
+
+    # Run non_max_suppression
+    result = non_max_suppression(
+        preds, n_classes, conf_thres=conf_thres, iou_thres=iou_thres, max_det=max_det
+    )
+
+    # The expected result should only include box1 and box3
+    assert len(result[0]) == 2
+    assert torch.allclose(result[0][0, :4], torch.tensor(box1[:4], dtype=torch.float32))
+    assert torch.allclose(result[0][1, :4], torch.tensor(box3[:4], dtype=torch.float32))

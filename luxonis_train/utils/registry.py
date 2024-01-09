d0333c3d489c7467b8146bdd15e2069d728ad3a0
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2024-01-09 18:24:14 +0100

    docs for registries

--- luxonis_train/utils/registry.py
+++ luxonis_train/utils/registry.py
@@ -4,11 +4,28 @@
 from luxonis_ml.utils.registry import Registry
 
 CALLBACKS = Registry(name="callbacks")
+"""Registry for all callbacks."""
+
 LOADERS = Registry(name="loaders")
+"""Registry for all loaders."""
+
 LOSSES = Registry(name="losses")
+"""Registry for all losses."""
+
 METRICS = Registry(name="metrics")
+"""Registry for all metrics."""
+
 MODELS = Registry(name="models")
+"""Registry for all models."""
+
 NODES = Registry(name="nodes")
+"""Registry for all nodes."""
+
 OPTIMIZERS = Registry(name="optimizers")
+"""Registry for all optimizers."""
+
 SCHEDULERS = Registry(name="schedulers")
+"""Registry for all schedulers."""
+
 VISUALIZERS = Registry(name="visualizers")
+"""Registry for all visualizers."""
